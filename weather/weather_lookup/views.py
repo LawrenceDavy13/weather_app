e5732ce3365354335ed3c2f2f1f1ae3a9881dfcf
LawrenceDavy13 <lawrencedavy13@gmail.com>
2020-06-09 18:39:49 +0100

    api and jumbotron added

--- weather/weather_lookup/views.py
+++ weather/weather_lookup/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'home.html', {})
+    import json, requests
+
+    # Get data from weather api
+    api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=10019&distance=5&API_KEY=08181FB1-D601-4C1F-8734-CA753616C2E5")
+
+    # Covert json api data to python object
+    try:
+        api = json.loads(api_requests.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api}) # Pass through api data to webpage
 
 
 def about(request):
